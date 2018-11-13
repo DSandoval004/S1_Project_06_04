90ae36d03b5dbf2c211e6d1fb364247d4ae1cc77
Diego Sandoval <dsandoval004@west-mec.org>
2018-11-13 15:27:57 -0700

    Views done
    Started transferring JavaScript from HTML to a separate file. Set up HTML. Moved some CSS around. Made the views look pretty and right.

---
+++ views.js
@@ -0,0 +1,35 @@
+function popUp1() {
+    document.getElementById('img1').style.display = 'block'
+}
+
+function popUp2() {
+    document.getElementById('img2').style.display = 'block'
+}
+
+function popUp3() {
+    document.getElementById('img3').style.display = 'block'
+}
+
+function popUp4() {
+    document.getElementById('img4').style.display = 'block'
+}
+
+function popUp5() {
+    document.getElementById('img5').style.display = 'block'
+}
+
+function popUp6() {
+    document.getElementById('img6').style.display = 'block'
+}
+
+function popUp7() {
+    document.getElementById('img7').style.display = 'block'
+}
+
+function popUp8() {
+    document.getElementById('img8').style.display = 'block'
+}
+
+function popUp9() {
+    document.getElementById('img9').style.display = 'block'
+}
