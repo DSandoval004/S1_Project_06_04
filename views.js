0f892447478cc9a1474f56cacb19315d7857179d
Diego Sandoval <dsandoval004@west-mec.org>
2018-11-14 13:28:26 -0700

    Needs comments: Finished JavaScript
    Put JavaScript in separate file and linked HTML properly to it.

--- views.js
+++ views.js
@@ -1,3 +1,4 @@
+// onClick to have popUp Happen
 function popUp1() {
     document.getElementById('img1').style.display = 'block'
 }
@@ -32,4 +33,112 @@ function popUp8() {
 
 function popUp9() {
     document.getElementById('img9').style.display = 'block'
+}
+// Cause Image to Appear when mouse is put over th li element
+function appear1() {
+    document.getElementById('photo1').style.display = 'block'
+}
+
+function appear2() {
+    document.getElementById('photo2').style.display = 'block'
+}
+
+function appear3() {
+    document.getElementById('photo3').style.display = 'block'
+}
+
+function appear4() {
+    document.getElementById('photo4').style.display = 'block'
+}
+
+function appear5() {
+    document.getElementById('photo5').style.display = 'block'
+}
+
+function appear6() {
+    document.getElementById('photo6').style.display = 'block'
+}
+
+function appear7() {
+    document.getElementById('photo7').style.display = 'block'
+}
+
+function appear8() {
+    document.getElementById('photo8').style.display = 'block'
+}
+
+function appear9() {
+    document.getElementById('photo9').style.display = 'block'
+}
+// Cause image to disappear when mouse is no longer hovered over the li element
+function disappear1() {
+    document.getElementById('photo1').style.display = 'none'
+}
+
+function disappear2() {
+    document.getElementById('photo2').style.display = 'none'
+}
+
+function disappear3() {
+    document.getElementById('photo3').style.display = 'none'
+}
+
+function disappear4() {
+    document.getElementById('photo4').style.display = 'none'
+}
+
+function disappear5() {
+    document.getElementById('photo5').style.display = 'none'
+}
+
+function disappear6() {
+    document.getElementById('photo6').style.display = 'none'
+}
+
+function disappear7() {
+    document.getElementById('photo7').style.display = 'none'
+}
+
+function disappear8() {
+    document.getElementById('photo8').style.display = 'none'
+}
+
+function disappear9() {
+    document.getElementById('photo9').style.display = 'none'
+}
+// Cause the popUp to be hiden after opening it
+function X1() {
+    document.getElementById('img1').style.display = 'none'
+}
+
+function X2() {
+    document.getElementById('img2').style.display = 'none'
+}
+
+function X3() {
+    document.getElementById('img3').style.display = 'none'
+}
+
+function X4() {
+    document.getElementById('img4').style.display = 'none'
+}
+
+function X5() {
+    document.getElementById('img5').style.display = 'none'
+}
+
+function X6() {
+    document.getElementById('img6').style.display = 'none'
+}
+
+function X7() {
+    document.getElementById('img7').style.display = 'none'
+}
+
+function X8() {
+    document.getElementById('img8').style.display = 'none'
+}
+
+function X9() {
+    document.getElementById('img9').style.display = 'none'
 }
